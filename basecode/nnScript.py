68bbba2fcc86d0efd8e5105ed41cc03a4de2e833
xcalvinsz <xcalvinsz@hotmail.com>
2015-03-05 22:18:39 -0500

    Update nnScript.py
    updated objfunc with dot

--- basecode/nnScript.py
+++ basecode/nnScript.py
@@ -166,23 +166,23 @@ def nnObjFunction(params, *args):
 	hidden_value = np.zeros((np.shape(training_data)[0],n_hidden+1))
 	
 	for j in range(np.shape(training_data)[0]):
-		for i in range(n_hidden):
-			for k in range(n_input):
-				hidden_value[j][i] += w1[i][k]*training_data[j][k]
+	   for i in range(n_hidden):
+	       for k in range(n_input):
+	           hidden_value[j][i] += w1[i][k]*training_data[j][k]
             
 	#hidden_value = np.dot(training_data, w1)
 				
 	#add bias node
 	for i in range (n_hidden):
-		for j in range (np.shape(training_data)[0]):
-			hidden_value[j][i] += 1 * w1[i][n_input]
+	    for j in range (np.shape(training_data)[0]):
+		hidden_value[j][i] += 1 * w1[i][n_input]
 
 	for j in range (np.shape(training_data)[0]):
-		hidden_value[j][n_hidden] = 1
+            hidden_value[j][n_hidden] = 1
 
 	for i in range(n_hidden):
-		for j in range (np.shape(training_data)[0]):
-			hidden_value[j][i] = sigmoid(hidden_value[j][i])
+	   for j in range (np.shape(training_data)[0]):
+		hidden_value[j][i] = sigmoid(hidden_value[j][i])
 
 
 
@@ -195,81 +195,109 @@ def nnObjFunction(params, *args):
 	
 	#output_value = np.ones((np.shape(training_data)[0], n_class))
 	output_value = np.dot(hidden_value, w2.T)
-	#output_value = np.dot(temp,output_value) 
+	#output_value = np.dot(temp,output_value)  
  
 	for i in range(n_class):
-		for k in range(np.shape(training_data)[0]):
-			output_value[k][i] = sigmoid(output_value[k][i])
+	    for k in range(np.shape(training_data)[0]):
+		output_value[k][i] = sigmoid(output_value[k][i])
 
-	# Now we do Error Function to find value of J
+        # Now we do Error Function to find value of J
 	J = np.zeros(np.shape(training_data)[0])
 	J_average = 0
 
 	
 	for i in range(np.shape(training_data)[0]):
-		for l in range(n_class):
-			J[i] += (-1.0)*(training_label[i][l]*np.log(output_value[i][l])+(1-training_label[i][l])*np.log(1-output_value[i][l]))
+	    for l in range(n_class):
+	        J[i] += (-1.0)*(training_label[i][l]*np.log(output_value[i][l])+(1-training_label[i][l])*np.log(1-output_value[i][l]))
 	for i in range(np.shape(training_data)[0]):
-		J_average += J[i]
+	    J_average += J[i]
 	J_average = J_average/np.shape(training_data)[0]
-	print (J_average)
+	print J_average
 	# Now compute the derivative of err function from hidden unit to output
 	#computing gradient for each weight and push them into hidden_err
 
-	########################################################################
+        ########################################################################
 
 
 	output_err = np.zeros((np.shape(training_data)[0],n_class,n_hidden+1))
 
 
 	for k in range(np.shape(training_label)[0]):
-		for l in range(n_class):
-			for j in range(n_hidden+1):
-				output_err[k][l][j] = (output_value[k][l] - training_label[k][l])*hidden_value[k][j]
+	    for l in range(n_class):
+		for j in range(n_hidden+1):
+		    output_err[k][l][j] = (output_value[k][l] - training_label[k][l])*hidden_value[k][j]
 
 	# Now compute the derivative of err function from input unit to output
 	hidden_err = np.zeros((np.shape(training_data)[0],n_hidden,n_input+1))
-	
+	'''
 	sigma_output = np.zeros((np.shape(training_label)[0],n_hidden))
 	for k in range(np.shape(training_label)[0]):
-		for l in range(0,n_class):
-			for j in range(0,n_hidden):
-				sigma_output[k][j] += w2[l][j] * (output_value[k][l] - training_label[k][l])
-
-	for k in range(np.shape(training_data)[0]):
-		for j in range(n_hidden):
-			for i in range(n_input):
-				hidden_err[k][j][i] = (1 - hidden_value[k][j]) * hidden_value[k][j] * sigma_output[k][j] * training_data[k][i]
-	
-	for k in range(np.shape(training_data)[0]):
-		for j in range(n_hidden):
-			hidden_err[k][j][n_input] = (1 - hidden_value[k][j]) * hidden_value[k][j] * sigma_output[k][j] * 1
-
-	grad_output = np.zeros((n_class,n_hidden+1))
-	
-	for k in range(np.shape(training_label)[0]):
-		for l in range(n_class):
-			for j in range(n_hidden+1):
-				grad_output[l][j] += output_err[k][l][j] / np.shape(training_label)[0]
-		
-	
-	grad_hidden = np.zeros((n_hidden,n_input+1))
-	
-	for k in range(np.shape(training_data)[0]):
-		for j in range(n_hidden):
-			for i in range(n_input+1):
-				grad_hidden[j][i] += hidden_err[k][j][i] / np.shape(training_data)[0]
+	    for l in range(0,n_class):
+	        for j in range(0,n_hidden):
+	            sigma_output[k][j] += w2[l][j] * (output_value[k][l] - training_label[k][l])
+	'''
+ 	'''print(w2)
+ 	print(output_value)
+  	print(training_label)
+   	print (sigma_output)'''
+   	train_temp = training_label[:,:n_class]
+   	sigma_output = np.dot((output_value-train_temp),w2)
+ 	#sigma_output = np.dot(w2,(output_value-training_label))
+   	
+   	for k in range(np.shape(training_data)[0]):
+            for j in range(n_hidden):
+                for i in range(n_input):
+                    hidden_err[k][j][i] = (1 - hidden_value[k][j]) * hidden_value[k][j] * sigma_output[k][j] * training_data[k][i]
+   	'''
+   	print (hidden_err)
+   	print ("hello")   
+   	print (hidden_value)
+   	print ("hello")  
+   	print (sigma_output)
+   	print ("hello")  
+   	print (training_data)
+   	  	   	
+   	for i in range(np.shape(training_data)[0]):
+   	   	temp1 = np.dot((1-hidden_value[i]),hidden_value[i].T)
+   	   	temp2 = np.dot(temp1, sigma_output[i])
+   	   	hidden_err[i] += np.dot(temp2, training_data)
+   	'''
+   	for k in range(np.shape(training_data)[0]):
+   	   	for j in range(n_hidden):
+   	   	   	hidden_err[k][j][n_input] = (1 - hidden_value[k][j]) * hidden_value[k][j] * sigma_output[k][j] * 1
         
-	obj_val = J_average
-		
+   	grad_output = np.zeros((n_class,n_hidden+1))
+   	''' 
+   	for k in range(np.shape(training_label)[0]):
+   	   	for l in range(n_class):
+   	   	   	for j in range(n_hidden+1):
+   	   	   	   	grad_output[l][j] += output_err[k][l][j] / np.shape(training_label)[0]
+		    '''
+   	for i in range(np.shape(training_label)[0]):
+   	   	grad_output += np.dot(output_err[i], 1/np.shape(training_label)[0])
+    
+   	
+   	#grad_output = output_err * (1/np.shape(training_label)[0]) 	
+   	      
+   	grad_hidden = np.zeros((n_hidden,n_input+1))
+   	'''  
+   	for k in range(np.shape(training_data)[0]):
+   	   	for j in range(n_hidden):
+   	   	   	for i in range(n_input+1):
+   	   	   	   	grad_hidden[j][i] += hidden_err[k][j][i] / np.shape(training_data)[0]
+   	'''  
+   	for i in range(np.shape(training_data)[0]):
+          grad_hidden += np.dot(hidden_err[i], 1/np.shape(training_data)[0])
+   	obj_val = J_average
+        
 
 	#Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
 	#you would use code similar to the one below to create a flat array
 	#obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
-	obj_grad = np.concatenate((grad_hidden.flatten(),grad_output.flatten()),0)
-	print (obj_grad)
+   	obj_grad = np.concatenate((grad_hidden.flatten(),grad_output.flatten()),0)
+   	print obj_grad
 
-	return (obj_val,obj_grad)
+   	return (obj_val,obj_grad)
 
 
 
